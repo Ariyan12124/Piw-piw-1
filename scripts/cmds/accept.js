9d660a18d9421a47a82de76e301fc4c83dcbc2f3
Ariyan12124 <cakeunivers6@gmail.com>
2025-10-25 02:27:56 +0600

    Update accept.js

--- scripts/cmds/accept.js
+++ scripts/cmds/accept.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "accept",
     aliases: ['acp'],
     version: "1.0",
-    author: "Loid Butter",
+    author: "gojo x zenitsu",
     countDown: 8,
     role: 2,
     shortDescription: "accept users",
